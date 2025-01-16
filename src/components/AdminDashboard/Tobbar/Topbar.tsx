d47f07ea0289b083738a11adff6ec29b0f522115
mdrony5134 <rony830388@gmail.com>
2025-01-16 17:16:58 +0600

    dashboard allmost finished

--- src/components/AdminDashboard/Tobbar/Topbar.tsx
+++ src/components/AdminDashboard/Tobbar/Topbar.tsx
@@ -1,14 +1,18 @@
 "use client";
-import profile from "@/asset/admin/pro.jpg";
+// import profile from "@/asset/admin/pro.jpg";
 import Image from "next/image";
 import hi from "@/asset/admin/hi.png";
-import { useState } from "react";
+import { useGetMeQuery } from "@/redux/Api/userApi";
+// import { useState } from "react";
 const Topbar = () => {
+  const { data } = useGetMeQuery({});
+  const userInformation = data?.data;
   return (
-    <header className="bg-white shadow py-4 px-16">
+    <header className="bg-white shadow py-4 px-16 z-[50]">
       <div className="flex justify-between items-center">
-        <h1 className="text-[#161616] flex items-center gap-2">
+        <h1 className="text-[#161616] flex items-center gap-2 text-[20px] font-medium">
           {/* Welcome Back, Admin!{" "} */}
+          Welcome, {userInformation.firstName} {userInformation.lastName}
           <Image className="w-6" src={hi} alt="welcome image" />
         </h1>
         <div>
