138dcdcf5c24ea4ac2d04674b4f70537fc22de0e
nirob019207 <nirobhasanbd01955@gmail.com>
2025-02-03 15:49:21 +0600

    cookis refrsh

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -79,11 +79,11 @@ function LoginForm() {
 
         // Decode the token to get role and other details
         const decoded: DecodedToken = jwtDecode(accessToken);
-        const { role, email } = decoded;
+        const { role, email } = decoded;  
 
         // Dispatch the user data to Redux
         dispatch(setUser({ role, token: accessToken, email }));
-        cookies.set("role", role, { expires: 7, path: "/" });
+        cookies.set("role", role, { expires: 7, path: "/" });  
 
         // Store the token in cookies
         cookies.set("token", accessToken, { expires: 7 });
